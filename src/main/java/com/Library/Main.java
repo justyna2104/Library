8de8052efc386a5339d65c85048ddb60522cbadb
justyna2104 <justynaharmuszkiewicz@gmail.com>
2021-11-08 16:39:02 +0100

    Added two windows in UI

--- src/main/java/com/Library/Main.java
+++ src/main/java/com/Library/Main.java
@@ -1,19 +1,70 @@
 package com.Library;
 
-import javax.persistence.EntityManager;
 import java.text.ParseException;
+import com.googlecode.lanterna.TerminalSize;
+import com.googlecode.lanterna.gui2.*;
+import com.googlecode.lanterna.gui2.dialogs.MessageDialog;
+import com.googlecode.lanterna.gui2.dialogs.MessageDialogButton;
+import com.googlecode.lanterna.screen.Screen;
+import com.googlecode.lanterna.terminal.DefaultTerminalFactory;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.TimeZone;
 
 public class Main {
-    public static void main(String[] args) throws ParseException {
+    public static void main(String[] args) throws ParseException, IOException {
 
-        BookService bookService = new BookService();
-        ReaderService readerService = new ReaderService();
-        CheckOutService checkOutService = new CheckOutService();
-        List<Reader> readers = checkOutService.whoIsOverDue();
-        for (Reader r:readers) {
-            System.out.println(r.toString());
-        }
+    DefaultTerminalFactory terminalFactory = new DefaultTerminalFactory();
+    Screen screen = terminalFactory.createScreen();
+    screen.startScreen();
+
+    final WindowBasedTextGUI textGUI = new MultiWindowTextGUI(screen);
+    final Window starterWindow = new BasicWindow("Starter Window");
+
+    Panel contentPanel = new Panel(new GridLayout(4));
+
+    GridLayout gridLayout = (GridLayout)contentPanel.getLayoutManager();
+    gridLayout.setHorizontalSpacing(10);
+    gridLayout.setTopMarginSize(1);
+
+    Label title = new Label("MAIN MANU");
+    title.setLayoutData(GridLayout.createLayoutData(
+           GridLayout.Alignment.CENTER,
+           GridLayout.Alignment.CENTER,
+           true,
+           false,
+           15,
+            10
+    ));
+    contentPanel.addComponent(title);
+
+    final Window searchBookByAuthorWindow = new BasicWindow("Search Window");
+    contentPanel.addComponent((new Label("Search books by author")));
+    contentPanel.addComponent(
+            new Button("Search", () -> textGUI.addWindowAndWait(searchBookByAuthorWindow)).setLayoutData(GridLayout.createLayoutData(GridLayout.Alignment.CENTER,GridLayout.Alignment.CENTER))
+    );
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    starterWindow.setComponent(contentPanel);
+    textGUI.addWindowAndWait(starterWindow);
 
     }
 }
